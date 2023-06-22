ef913f061cac2ffdcc64d2f31e7ad95030ef4101
Ragujan Rag JN <stiflerwedontgiveup@gmail.com>
2023-06-22 12:01:36 +0530

    add: sidebar responsiveness is added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,44 +2,64 @@ import React, { useEffect, useRef, useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 function Dashboard() {
-  const [sideBarWidth, setSideBarWidth] = useState(0);
+  const [sideBarState, setSideBarState] = useState(false);
   const sideBarRef = useRef(null);
 
   const [settingIconState, setSettingIconState] = useState(false);
   const [settingOptionHeight, setSettingOptionHeight] = useState(0);
   const settingOptionDiv = useRef(null);
-
   const [chartIconState, setChartIconState] = useState(false);
-  const [sideBarState, setSideBarState] = useState(false);
-
 
   const bodySectionRef = useRef(null);
 
   let sideBarClassList =
-    "flex-col py-5 md:w-[250px] hidden md:flex gap-y-3 transistion duration-300";
+    "flex-col py-5   md:flex gap-y-3 transistion duration-300";
+
+  const sideBarIconsOnly = (sideBar) => {
+    const childDivs = sideBar.childNodes;
+
+    childDivs.forEach((element) => {
+      let subDivs = element.childNodes;
+      subDivs.forEach((subDiv) => {
+        let singleSubDiv = subDiv.childNodes;
+
+        let isFirstSvg = true;
+        singleSubDiv.forEach((subElement) => {
+          if (subElement.nodeName === "svg" && isFirstSvg) {
+            isFirstSvg = false;
+            return;
+          }
+          console.log("side bar status was ",sideBarState)
+          if (sideBarState && window.innerWidth > 640) {
+            subElement.style.display = "none";
+          } else if (sideBarState && window.innerWidth <= 640) {
+            sideBarRef.current.style.display = "";
+            subElement.style.display = "";
+          } else if (!sideBarState && window.innerWidth > 640) {
+            subElement.style.display = "";
+            sideBarRef.current.style.display = "";
+          } else if (!sideBarState && window.innerWidth <= 640) {
+            sideBarRef.current.style.display = "none";
+            subElement.style.display = "";
+          }else{
+            console.log("else statement ")
+          }
+        });
+      });
+    });
+  };
 
   useEffect(() => {
     if (settingOptionHeight === 0) {
       setSettingOptionHeight(settingOptionDiv.current.scrollHeight);
     }
-    if (sideBarWidth === 0) {
-      let value = bodySectionRef.current.querySelector('div:first-child').getBoundingClientRect().width;
-
-      let paddingLeft = parseInt(window.getComputedStyle(bodySectionRef.current).paddingLeft);
-      let paddingRight = parseInt(window.getComputedStyle(bodySectionRef.current).paddingRight);
-
-      setSideBarWidth(sideBarRef.current.offsetWidth+2*(paddingLeft+paddingRight))
-
-    }
-    console.log("setting option height is " + settingOptionHeight);
-    console.log("side bar with is ", sideBarWidth);
   }, [settingOptionHeight, sideBarState]);
   useEffect(() => {
-    if (sideBarState) {
-      sideBarRef.current.style.marginLeft = `-${sideBarWidth}px`;
-    } else {
-      sideBarRef.current.style.marginLeft = `0px`;
-    }
+    sideBarIconsOnly(sideBarRef.current);
+
+    window.addEventListener('resize',()=>{
+      sideBarIconsOnly(sideBarRef.current)
+    })
   }, [sideBarState]);
 
   return (
@@ -109,26 +129,34 @@ function Dashboard() {
         </div>
       </div>
       {/* body section */}
-      <div ref={bodySectionRef} className="flex flex-row px-3 bg-main-blue">
+      <div
+        ref={bodySectionRef}
+        className="relative flex flex-row px-3 bg-main-blue"
+      >
         {/* sidebar */}
-        <div ref={sideBarRef} className={`${sideBarClassList}`}>
+        <div
+          ref={sideBarRef}
+          className={`${sideBarClassList} absolute md:static top-0 left-0 bg-slate-900 px-2 md:px-0 md:bg-main-blue`}
+        >
           <div className="flex flex-row items-center px-3 py-2 rounded-md cursor-pointer bg-light-darker bg-opacity-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-8 h-8 text-white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-              />
-            </svg>
+            <div className="flex flex-row items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-8 h-8 text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
 
-            <h4 className="px-10 font-bold text-white">Home</h4>
+              <h4 className="px-10 font-bold text-white">Home</h4>
+            </div>
           </div>
           <div
             onClick={() => {
@@ -207,22 +235,24 @@ function Dashboard() {
           </div>
 
           <div className="flex flex-row items-center px-3 py-2 rounded-md cursor-pointer bg-light-darker bg-opacity-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-8 h-8 text-white"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
-              />
-            </svg>
+            <div className="flex flex-row items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-8 h-8 text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+                />
+              </svg>
 
-            <h4 className="px-10 font-bold text-white">Elements</h4>
+              <h4 className="px-10 font-bold text-white">Elements</h4>
+            </div>
           </div>
 
           <div
